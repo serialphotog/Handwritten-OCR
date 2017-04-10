374292c973e0ef3cefe2b1bf92ca406ba5ea5473
Adam Thompson <adam@serialphotog.com>
2017-04-10 16:30:23 -0400

    Added ability to centrally align sample images

--- Util/handwriting_sample.py
+++ Util/handwriting_sample.py
@@ -3,6 +3,8 @@ import math
 import numpy as np 
 import os
 
+from scipy import ndimage
+
 # Square size to resize images to
 IMAGE_RESIZE = 28
 
@@ -25,6 +27,9 @@ def get_sample_image_data(path):
 		# We want a white image on a black background
 		(threshold, gray_scale) = cv2.threshold(gray_scale, 128, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)
 
+		# Get our image more centrally aligned
+		gray_scale = shift_image(gray_scale)
+
 		# Flatten the image:
 		# Our neural network expects that our pixel values be in the range 0-1, not 0-255
 		flattened = gray_scale.flatten() / 255.0
@@ -43,3 +48,19 @@ def get_sample_image_data(path):
 		correct_vals[i] = correct_val
 
 	return image_data, correct_vals
+
+# Shifts image to be more center aligned
+# The openCV documentation is very helpful for figuring this out:
+#	http://docs.opencv.org/master/da/d6e/tutorial_py_geometric_transformations.html#gsc.tab=0
+def shift_image(image):
+	# Calculate the shift amount
+	cy, cx = ndimage.measurements.center_of_mass(image)
+	rows, cols = image.shape
+	shiftx = np.round(cols/2.0-cx).astype(int)
+	shifty = np.round(rows/2.0-cy).astype(int)
+
+	# Build the transformation matrix and perform the shift
+	transform = np.float32([[1,0,shiftx],[0,1,shifty]])
+	shifted = cv2.warpAffine(image, transform, (cols, rows))
+
+	return shifted
