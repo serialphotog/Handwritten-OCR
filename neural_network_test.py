fe024e2666c594758e53e3bb35c955e2ad4d711e
Adam Thompson <adam@serialphotog.com>
2017-04-21 21:19:35 -0400

    Improved main runtime environment

--- neural_network_test.py
+++ neural_network_test.py
@@ -4,27 +4,44 @@ from Core.nn.Model import Model
 from Core.nn.NeuralNetwork import NeuralNetwork
 from Core.data.NNData import NNData
 
-def main():
-	# Handle command line arguments
-	parser = argparse.ArgumentParser(description="Neural Network Tester")
-	parser.add_argument("-v", action="store_true", dest="verbose", 
-		help="Enables verbose output.")
-	parser.add_argument("-g", action="store_true", dest="enable_graph",
-		help="Enables graph production.")
-	args = parser.parse_args()
+class App:
+	##########
+	# Initialize application components
+	##########
+	def __init__(self):
+		# Parse command line arguments
+		self.__parse_args()
 
-	# Build a new model
-	model = Model.new_model([784, 500, 10])
+		# Initialize the data engine
+		self.data_engine = NNData()
 
-	# Our data processor
-	data_engine = NNData()
+	##########
+	# Runs the app
+	##########
+	def run(self):
+		model = Model.new_model([784, 500, 10])
 
-	# Start the testing
-	neural_network = NeuralNetwork(model, epochs=10, verbose=args.verbose, 
-		enable_graph=args.enable_graph)
-	neural_network.train_mnist()
-	#images, correct_vals = data_engine.load_local_data("/home/adam/projects/handwriting-samples/raw/")
-	#Sneural_network.test(images, correct_vals)
+		# Start the testing
+		neural_network = NeuralNetwork(model, epochs=10, verbose=self.args.verbose, 
+			enable_graph=self.args.enable_graph)
+		neural_network.train_mnist()
+		#images, correct_vals = data_engine.load_local_data("/home/adam/projects/handwriting-samples/raw/")
+		#Sneural_network.test(images, correct_vals)
 
+
+	##########
+	# Parses the command line arguments
+	##########
+	def __parse_args(self):
+		parser = argparse.ArgumentParser(description="Neural Network Tester")
+		parser.add_argument("-v", action="store_true", dest="verbose", 
+			help="Enables verbose output.")
+		parser.add_argument("-g", action="store_true", dest="enable_graph",
+			help="Enables graph production.")
+		self.args = parser.parse_args()
+
+
+# Run everything
 if __name__ == '__main__':
-	main()
+	app = App()
+	app.run()
